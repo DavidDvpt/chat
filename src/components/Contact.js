e392111854938bdc1e80ca2f29420294995ac518
DavidDvpt <david.mosca69@gmail.com>
2020-10-12 12:42:30 +0200

    add contact.js and clear app.js

---
+++ src/components/Contact.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Contact.css';
+
+function Contact () {
+    return ''
+}
+
+export default Contact
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// https://randomuser.me/api/portraits/men/79.jpg
