0dc85f13de3a15b29b36ce92005535094a6f342f
DavidDvpt <david.mosca69@gmail.com>
2020-10-13 12:32:32 +0200

    final version of quest

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import './Contact.css';
+import React from "react";
+import PropTypes from "prop-types";
+import "./Contact.css";
 
-function Contact () {
-    return <div>dudul</div>
+function Contact(props) {
+
+    return (
+        <div className="Contact">
+            <div className="avatar">
+                <img className="avatar" src={props.avatar} alt="my Face" />
+            </div>
+            <div>
+                <h4 className="name">{props.name}</h4>
+                <p className="status">
+                    <span className={props.online ? "status-online" : "status-offline"} />
+                    <span className="status-text">
+                        {" "}
+                        {props.online ? "online" : "offline"}{" "}
+                    </span>
+                </p>
+            </div>
+        </div>
+    );
 }
 
-export default Contact
+Contact.propTypes = {
+    avatar: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    online: PropTypes.bool.isRequired
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// https://randomuser.me/api/portraits/men/79.jpg
+export default Contact;
