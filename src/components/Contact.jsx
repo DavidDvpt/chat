5a367ed521081f350f3248c5b06a4538c4cff277
DavidDvpt <david.mosca69@gmail.com>
2020-10-14 11:20:55 +0200

    transform the the functionnal component en state class

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,25 +2,38 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Contact.css";
 
-function Contact(props) {
+class Contact extends React.Component {
 
-    return (
-        <div className="Contact">
-            <div className="avatar">
-                <img className="avatar" src={props.avatar} alt="my Face" />
+    constructor(props) {
+        super(props);
+        this.state = {
+            online: this.props.online
+        }
+    }
+    render() {
+        const onlineListener = (event) => {
+            this.setState({
+                online: !this.state.online
+            })
+        }
+        return (
+            <div className="Contact">
+                <div className="avatar">
+                    <img className="avatar" src={this.props.avatar} alt="my Face" />
+                </div>
+                <div>
+                    <h4 className="name">{this.props.name}</h4>
+                    <p className="status" onClick={onlineListener}>
+                        <span className={this.state.online ? "status-online" : "status-offline"} />
+                        <span className="status-text">
+                            {" "}
+                            {this.state.online ? "online" : "offline"}{" "}
+                        </span>
+                    </p>
+                </div>
             </div>
-            <div>
-                <h4 className="name">{props.name}</h4>
-                <p className="status">
-                    <span className={props.online ? "status-online" : "status-offline"} />
-                    <span className="status-text">
-                        {" "}
-                        {props.online ? "online" : "offline"}{" "}
-                    </span>
-                </p>
-            </div>
-        </div>
-    );
+        );
+    }
 }
 
 Contact.propTypes = {
